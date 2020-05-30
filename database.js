57f4349e1234bfe1dc061810af4c620bf1ae635f
FlyN Nick <nickassader@gmail.com>
2020-05-30 13:46:02 -0700

    Hopefully atlas works now, but atleast it'll fall back to heroku if it doesn't

--- database.js
+++ database.js
@@ -3,22 +3,6 @@ let ChatRoomModel = require('./schema/chatRoom');
 let IDTrackerModel = require('./schema/idTracker');
 let EndrosementModel = require('./schema/endorsement');
 
-const atlasUsername = 'FlyNNick'
-const atlasPassword = 'yOiR5KZV7Sa36ANE'
-const newAtlasUsername = 'FlyNNickNonAdmin'
-const newAtlasPassword = 'ab4Rgpkl9egy16cV'
-const atlasShell = 'mongo "mongodb+srv://chatn-xjdcp.mongodb.net/test" --username FlyNNick'
-//authSource=admin&
-const atlasURI = 'mongodb+srv://${atlasUsername}:${atlasPassword}@chatn-xjdcp.mongodb.net/test?retryWrites=true&w=majority'
-const atlasOldURI = 'mongodb://${newAtlasUsername}:${newAtlasPassword}@chatn-shard-00-00-xjdcp.mongodb.net:27017,chatn-shard-00-01-xjdcp.mongodb.net:27017,chatn-shard-00-02-xjdcp.mongodb.net:27017/test?ssl=true&replicaSet=ChatN-shard-0&authSource=admin&retryWrites=true&w=majority'
-const localURI = 'mongodb://localhost:27017/chattingLocal'
-const local = false;
-
-function connectToWhere()
-{
-	if (local) { return localURI }
-	else { return process.env.MONGODB_URI }
-}
 class Database 
 {
 	constructor()    
@@ -28,7 +12,7 @@ class Database
 
 	_connect()
 	{
-		mongoose.connect(connectToWhere(), { useUnifiedTopology: true, useNewUrlParser: true })
+		mongoose.connect(process.env.ATLAS_ADMIN_URI || process.env.ATLAS_NONADMIN_URI || process.env.MONGODB_URI || localURI , { useUnifiedTopology: true, useNewUrlParser: true })
 			.then(() => 
 			{
 				console.log('Database connection successful');
