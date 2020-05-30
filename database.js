ad24d96ddc27b7681150db58addf294335a6b71c
FlyN Nick <nickassader@gmail.com>
2020-05-30 13:53:16 -0700

    Hopefully final commit, I have decided to not use atlas because it kept on giving me the whitelisting error even though I have whitelisted all ip addresses...

--- database.js
+++ database.js
@@ -3,6 +3,11 @@ let ChatRoomModel = require('./schema/chatRoom');
 let IDTrackerModel = require('./schema/idTracker');
 let EndrosementModel = require('./schema/endorsement');
 
+const atlasAdminURI = process.env.ATLAS_ADMIN_URI;
+const atlasNonAdminURI = process.env.ATLAS_NONADMIN_URI;
+const herokuURI = process.env.MONGODB_URI;
+const localURI = 'mongodb://localhost:27017/chattingLocal';	
+
 class Database 
 {
 	constructor()    
@@ -12,7 +17,14 @@ class Database
 
 	_connect()
 	{
-		mongoose.connect(process.env.ATLAS_ADMIN_URI || process.env.ATLAS_NONADMIN_URI || process.env.MONGODB_URI || localURI , { useUnifiedTopology: true, useNewUrlParser: true })
+		// the atlas uri's are commented out because they would not work
+		// I would get the error, even though I have whitelisted all ip addresses: 
+		/*
+		 * MongooseServerSelectionError: Could not connect to any servers in your MongoDB Atlas cluster. 
+		 * One common reason is that you're trying to access the database from an IP that isn't whitelisted. 
+		 * Make sure your current IP address is on your Atlas cluster's IP whitelist: https://docs.atlas.mongodb.com/security-whitelist/
+		 */
+		mongoose.connect(/*process.env.ATLAS_ADMIN_URI || process.env.ATLAS_NONADMIN_URI || */process.env.MONGODB_URI || localURI , { useUnifiedTopology: true, useNewUrlParser: true })
 			.then(() => 
 			{
 				console.log('Database connection successful');
