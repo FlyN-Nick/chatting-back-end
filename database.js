b4aa160aad83c558940a540ddbd522ad778d13f9
FlyN Nick <nickassader@gmail.com>
2020-05-30 13:18:00 -0700

    Trying a different mongodb atlas user

--- database.js
+++ database.js
@@ -5,10 +5,12 @@ let EndrosementModel = require('./schema/endorsement');
 
 const atlasUsername = 'FlyNNick'
 const atlasPassword = 'yOiR5KZV7Sa36ANE'
+const newAtlasUsername = 'FlyNNickNonAdmin'
+const newAtlasPassword = 'ab4Rgpkl9egy16cV'
 const atlasShell = 'mongo "mongodb+srv://chatn-xjdcp.mongodb.net/test" --username FlyNNick'
 //authSource=admin&
 const atlasURI = 'mongodb+srv://${atlasUsername}:${atlasPassword}@chatn-xjdcp.mongodb.net/test?retryWrites=true&w=majority'
-const atlasOldURI = 'mongodb://${atlasUsername}:${atlasPassword}@chatn-shard-00-00-xjdcp.mongodb.net:27017,chatn-shard-00-01-xjdcp.mongodb.net:27017,chatn-shard-00-02-xjdcp.mongodb.net:27017/test?ssl=true&replicaSet=ChatN-shard-0&authSource=admin&retryWrites=true&w=majority'
+const atlasOldURI = 'mongodb://${newAtlasUsername}:${newAtlasPassword}@chatn-shard-00-00-xjdcp.mongodb.net:27017,chatn-shard-00-01-xjdcp.mongodb.net:27017,chatn-shard-00-02-xjdcp.mongodb.net:27017/test?ssl=true&replicaSet=ChatN-shard-0&authSource=admin&retryWrites=true&w=majority'
 const localURI = 'mongodb://localhost:27017/chattingLocal'
 const local = false;
 
