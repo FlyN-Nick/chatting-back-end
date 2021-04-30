146b6ae83031c595373b96f9c75013980906053e
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2021-04-30 15:02:24 -0700

    Got it to successfully compile.

---
+++ dist/database.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = require("mongoose");
+// was previously using Heroku's mLab add-on, mLab got migrated to atlas
+var atlasURI = process.env.MONGODB_URI; // proccess.env to acess heroku config vars 
+var localURI = 'mongodb://localhost:27017/chattingLocal';
+var Database = /** @class */ (function () {
+    function Database() {
+        this._connect();
+    }
+    Database.prototype._connect = function () {
+        // if the backend is being run locally it'll just use the variable localURI 
+        mongoose_1.connect(atlasURI || localURI, { useUnifiedTopology: true, useNewUrlParser: true })
+            .then(function () { console.log('Database connection successful'); })
+            .catch(function (err) {
+            console.error('Database connection error:');
+            console.error(err);
+        });
+    };
+    return Database;
+}());
+module.exports = new Database();
+//# sourceMappingURL=database.js.map
